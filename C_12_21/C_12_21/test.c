d7771f751d21fc4371b8d487f9b3357baad74069
dodo <sxnico@outlook.com>
2021-12-23 23:14:15 +0800

    12_23

--- C_12_21/C_12_21/test.c
+++ C_12_21/C_12_21/test.c
@@ -3,43 +3,78 @@
 #include<string.h>
 #include<stdlib.h>
 
-int maxScore(int* cardPoints, int cardPointsSize, int k)
+char** fizzBuzz(int n, int* returnSize)
 {
-	int sum = 0;
-	int len = cardPointsSize - k;
-	int sumlen = 0;
-	for (int i = 0; i < cardPointsSize; i++)
-	{
-		sum += cardPoints[i];
-	}
-	for (int i = 0; i < len; i++)
-	{
-		sumlen += cardPoints[i];
-	}
-	if (len == 0)
-		return sum;
-	int min = sumlen;
-	int l = 0;
-	int r = len;
-	while (r < cardPointsSize)
-	{
-		sumlen += cardPoints[r];
-		sumlen -= cardPoints[l];
-		r++;
-		l++;
-		if (min > sumlen)
-			min = sumlen;
-	}
-	return sum - min;
+    *returnSize = 0;
+    char** ret = (char**)malloc(sizeof(char*) * n);
+    for (int i = 0; i < n; i++)
+    {
+        ret[i] = (char*)malloc(sizeof(char) * 9);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            strcpy(ret[i - 1], "FizzBuzz");
+        }
+        else if (i % 3 == 0)
+            strcpy(ret[i - 1], "Fizz");
+        else if (i % 5 == 0)
+            strcpy(ret[i - 1], "Buzz");
+        else
+            sprintf(ret[i - 1], "%d", i);
+        (*returnSize)++;
+    }
+    return ret;
 }
+
 int main()
 {
-	int arr[] = { 1, 79, 80, 1, 1, 1, 200, 1 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	int ret = maxScore(arr, sz, 3);
-	printf("%d", ret);
-	return 0;
+    int sz = 1;
+    char** ret = fizzBuzz(3, &sz);
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%s", ret[i]);
+    }
+    return 0;
 }
+//int maxScore(int* cardPoints, int cardPointsSize, int k)
+//{
+//	int sum = 0;
+//	int len = cardPointsSize - k;
+//	int sumlen = 0;
+//	for (int i = 0; i < cardPointsSize; i++)
+//	{
+//		sum += cardPoints[i];
+//	}
+//	for (int i = 0; i < len; i++)
+//	{
+//		sumlen += cardPoints[i];
+//	}
+//	if (len == 0)
+//		return sum;
+//	int min = sumlen;
+//	int l = 0;
+//	int r = len;
+//	while (r < cardPointsSize)
+//	{
+//		sumlen += cardPoints[r];
+//		sumlen -= cardPoints[l];
+//		r++;
+//		l++;
+//		if (min > sumlen)
+//			min = sumlen;
+//	}
+//	return sum - min;
+//}
+//int main()
+//{
+//	int arr[] = { 1, 79, 80, 1, 1, 1, 200, 1 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int ret = maxScore(arr, sz, 3);
+//	printf("%d", ret);
+//	return 0;
+//}
 //int maxScore(int* cardPoints, int cardPointsSize, int k)
 //{
 //	int sum = 0;
