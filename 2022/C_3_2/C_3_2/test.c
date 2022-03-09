180e72413f7e8352c4a8ce298427bfdccdb1e967
dodo <sxnico@outlook.com>
2022-03-09 21:12:40 +0800

    3_9

--- 2022/C_3_2/C_3_2/test.c
+++ 2022/C_3_2/C_3_2/test.c
@@ -1,39 +1,51 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#define PRINT(symbol1,symbol2) symbol1##symbol2 
+#define PRINT1(value, symbol) printf("the" #symbol "is value");
 
-int* singleNumbers(int* nums, int numsSize, int* returnSize) 
+int main()
 {
-    int i = 0;
-    int orsum = 0;
-    for (i = 0; i < numsSize; i++)
-        orsum ^= nums[i];
-    int lowpos = orsum - (orsum & (orsum - 1));
-    int result1 = 0;
-    int result2 = 0;
-    for (i = 0; i < numsSize; i++)
-    {
-        if (lowpos & nums[i])
-            result1 ^= nums[i];
-        else
-            result2 ^= nums[i];
-    }
-    int* ret = (int*)malloc(sizeof(int) * 2);
-    *returnSize = 0;
-    ret[(*returnSize)++] = result1;
-    ret[(*returnSize)++] = result2;
-    return ret;
+    int a = 0;
+    int b = 0;
+    int ab = 0;
+    //PRINT(a, b) = 2;
+    PRINT1(1, AB);
+    //printf("%d", ab);
+    return 0;
 }
-
-
-int missingNumber(int* nums, int numsSize)
-{
-    int ret = 0;
-    for (int i = 0; i < numsSize + 1; i++)
-    {
-        ret ^= nums[i];
-    }
-    return ret;
-}
+//int* singleNumbers(int* nums, int numsSize, int* returnSize) 
+//{
+//    int i = 0;
+//    int orsum = 0;
+//    for (i = 0; i < numsSize; i++)
+//        orsum ^= nums[i];
+//    int lowpos = orsum - (orsum & (orsum - 1));
+//    int result1 = 0;
+//    int result2 = 0;
+//    for (i = 0; i < numsSize; i++)
+//    {
+//        if (lowpos & nums[i])
+//            result1 ^= nums[i];
+//        else
+//            result2 ^= nums[i];
+//    }
+//    int* ret = (int*)malloc(sizeof(int) * 2);
+//    *returnSize = 0;
+//    ret[(*returnSize)++] = result1;
+//    ret[(*returnSize)++] = result2;
+//    return ret;
+//}
+//
+//
+//int missingNumber(int* nums, int numsSize)
+//{
+//    int ret = 0;
+//    for (int i = 0; i < numsSize + 1; i++)
+//    {
+//        ret ^= nums[i];
+//    }
+//    return ret;
+//}
 
 //#define _CRT_SECURE_NO_WARNINGS 1
 //#include<stdio.h>
